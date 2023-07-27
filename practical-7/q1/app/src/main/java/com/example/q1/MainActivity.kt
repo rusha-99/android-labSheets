2015d06adf364bd756ec2dcca9e42933d6ea1e08
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-07-28 00:18:22 +0530

    finish question one

--- practical-7/q1/app/src/main/java/com/example/q1/MainActivity.kt
+++ practical-7/q1/app/src/main/java/com/example/q1/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.example.q1
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+// Set an onClickListener for the button
+        button.setOnClickListener {
+
+            // Create a string variable with the web URL
+            val url = "https://github.com/rusha-99"
+
+            // Create an Intent object with the action ACTION_VIEW and the data as the web URL
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+
+            // Start the activity with the intent
+            startActivity(intent)
+        }
+
+
     }
 }
