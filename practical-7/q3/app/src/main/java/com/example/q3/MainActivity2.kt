8096a774a59f080d0f7deb8a28162d208f79c10d
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-04 22:34:44 +0530

    edit

--- practical-7/q3/app/src/main/java/com/example/q3/MainActivity2.kt
+++ practical-7/q3/app/src/main/java/com/example/q3/MainActivity2.kt
@@ -1,5 +1,6 @@
 package com.example.q3
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,16 +8,18 @@ import android.widget.Button
 import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
-        val receivedMessageTextView: TextView = findViewById(R.id.textView)
+        val receivedMessageTextView: TextView = findViewById(R.id.textViewThree)
 
         // Receive the message from the intent
         val message = intent.getStringExtra("EXTRA_MESSAGE")
 
         // Display the received message in MainActivity2
+        //receivedMessageTextView.text = message
         receivedMessageTextView.text = message
 
         val button = findViewById<Button>(R.id.buttonTwo)
