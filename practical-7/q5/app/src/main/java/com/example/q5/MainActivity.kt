2c3693b78ddba8db87cd1a5499e215160ebb5e35
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-06 15:54:13 +0530

    update

--- practical-7/q5/app/src/main/java/com/example/q5/MainActivity.kt
+++ practical-7/q5/app/src/main/java/com/example/q5/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.q5
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if(Buid.VERSION)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+            0
+        )
+//        setContentView(R.layout.activity_main)
+        setContentView{
+
+        }
     }
+
+
 }
---
+++ practical-7/q5/app/src/main/java/com/example/q5/RunningService.kt
@@ -0,0 +1,35 @@
+package com.example.q5
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import androidx.core.app.NotificationCompat
+
+class RunningService: Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        when(intent?.action){
+            Actions.START.toString() ->start()
+            Actions.STOP.toString() ->stopSelf()
+
+        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun start(){
+        val notification = NotificationCompat.Builder(this, "running_channel")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Run is active")
+            .setContentTitle("Elapsed time: 00:50")
+            .build()
+        startForeground(1,notification)
+    }
+
+    enum class Actions{
+        START, STOP
+
+    }
+}
