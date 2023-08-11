26f13025ac265a4da55f6426689b14264181445a
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-11 23:38:11 +0530

    extends `SQLiteOpenHelper

--- practical-8/q1/app/src/main/java/com/example/q1/database/DatabaseHelper.kt
+++ practical-8/q1/app/src/main/java/com/example/q1/database/DatabaseHelper.kt
@@ -1,4 +1,21 @@
 package com.example.q1.database
 
-class DatabaseHelper {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context):SQLiteOpenHelper(context, DB_Name,null, DB_VERSION) {
+
+    companion object {
+        private val DB_Name = "task"
+        private  val DB_VERSION = 1
+    }
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
 }
