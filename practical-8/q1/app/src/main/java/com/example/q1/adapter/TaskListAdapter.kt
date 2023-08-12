098c022a20d5c85b93c02964137d82e9e52f64ac
Tharusha Pathirana <91452474+rusha-99@users.noreply.github.com>
2023-08-12 21:00:23 +0530

    update

--- practical-8/q1/app/src/main/java/com/example/q1/adapter/TaskListAdapter.kt
+++ practical-8/q1/app/src/main/java/com/example/q1/adapter/TaskListAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.q1.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.q1.R
 import com.example.q1.model.TaskListModel
@@ -18,17 +21,28 @@ class TaskListAdapter(tasklist:List<TaskListModel>,internal var context: Context
         this.tasklist = tasklist
     }
 
-    inner class TaskViewHolder(view :View) :RecyclerView.ViewHolder(view)
+    inner class TaskViewHolder(view :View) :RecyclerView.ViewHolder(view){
+        var name: TextView = view.findViewById(R.id.txt_name)
+        var details: TextView = view.findViewById(R.id.txt_details)
+        var btn_edit: Button = view.findViewById(R.id.btn_edit)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.recycler_task_list,parent,false)
+        return TaskViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return  tasklist.size
     }
 
     override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val tasks =tasklist[position]
+        holder.name.text = tasks.name
+        holder.details.text = tasks.details
+
+        holder.btn_edit.setOnClickListener {
+            val i = Intent(context,AddTask ::class.java)
+        }
     }
 }
